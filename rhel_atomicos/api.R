9c60bb1a7c9062eabe0141d6d7a3e483716aa44f
FvD <FvD@users.noreply.github.com>
2018-05-29 23:54:22 -0600

    añade ejemplo rhel atomic os

---
+++ rhel_atomicos/api.R
@@ -0,0 +1,93 @@
+library(plumber)
+library(jsonlite)
+
+# ------------- GET------------------
+#* @get /
+function() {
+    ejemplo <- fromJSON(
+    '{"fontanero" : "RHEL Atomic"}'
+    )
+  return(ejemplo)
+}
+
+#* @get /1
+function() {
+    ejemplo <- fromJSON(
+    '{"get" : "sin acentos"}'
+    )
+  return(ejemplo)
+}
+
+#* @get /2
+function() {
+    ejemplo <- fromJSON(
+    '{"get" : "ñ é í ó - algunos ejemplos"}'
+    )
+  return(ejemplo)
+}
+
+#* @get /3
+function() {
+  `valór_ácentos` <- 23
+  ejemplo <- `valór_ácentos`   
+  return(ejemplo)
+}
+
+#* @get /4
+function() {
+  `valór_ácentos` <- 23
+  ejemplo <- `valór_ácentos`   
+  return(ejemplo)
+}
+
+#* @get /4
+function() {
+  `valór_ácentos` <- 46
+  ejemplo <- `valór_ácentos`   
+  return(ejemplo)
+}
+
+# ------------- POST ------------------
+#* @post /5
+function(
+  data
+) {
+  ejemplo <- data
+  return(ejemplo)
+}
+
+#* @post /6
+function(
+  data
+) {
+  ejemplo <- "ñ é í ó - algunos ejemplos"
+  return(ejemplo)
+}
+
+#* @post /7
+function(
+  data
+  ) {
+  `valór_ácentos` <- 23 * data
+  ejemplo <- `valór_ácentos`   
+  return(ejemplo)
+}
+
+#* @post /8
+function(
+  data
+  ) {
+  `valór_ácentos` <- 23 * data
+  ejemplo <- `valór_ácentos`   
+  return(ejemplo)
+}
+
+#* @post /8
+function(
+  data
+  ) {
+  `valór_ácentos` <- 46 * data
+  ejemplo <- `valór_ácentos`   
+  return(ejemplo)
+}
+
